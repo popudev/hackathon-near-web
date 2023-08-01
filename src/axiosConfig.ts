ba52394ec2b3c4df2dcb34dfe8264f00b04c3b0d
KidP2H <thjnhsoajca@gmail.com>
2023-08-01 23:52:01 +0700

    refactor: axios config

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,19 +1,7 @@
 import axios from "axios";
 
-console.log(`base url: ${process.env.NEXT_PUBLIC_BASEURL_SERVER}`);
 const instance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_BASEURL_SERVER}`
+  baseURL: `${process.env.NEXT_PUBLIC_BASEURL_SERVER}`,
 });
-
-// instance.interceptors.request.use((config) => {
-//   if (accessToken) {
-//     config.headers.Authorization = `Bearer ${accessToken}`;
-//   }
-
-//   if (isServer() && context?.req?.cookies) {
-//     config.headers.Cookie = `gid=${context.req.cookies.gid};`;
-//   }
-//   return config;
-// });
 
 export default instance;
