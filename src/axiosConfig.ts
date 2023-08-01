61952653260e7f3a2b9c28069d0ce3261d639b9e
KidP2H <thjnhsoajca@gmail.com>
2023-08-01 19:01:52 +0700

    feat: setup react-quert with axios

---
+++ src/axiosConfig.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+console.log(`base url: ${process.env.NEXT_PUBLIC_BASEURL_SERVER}`);
+const instance = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_BASEURL_SERVER}`,
+  withCredentials: true,
+});
+
+// instance.interceptors.request.use((config) => {
+//   if (accessToken) {
+//     config.headers.Authorization = `Bearer ${accessToken}`;
+//   }
+
+//   if (isServer() && context?.req?.cookies) {
+//     config.headers.Cookie = `gid=${context.req.cookies.gid};`;
+//   }
+//   return config;
+// });
+
+export default instance;
