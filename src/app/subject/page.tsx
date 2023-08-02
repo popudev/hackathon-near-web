dd7fe4797203e0df6854eb5ca1f17bdd379233e1
Bùi Mạnh Thành TTS <manhthanh147@gmail.com>
2023-08-02 21:42:50 +0700

    fix:remove

--- src/app/subject/page.tsx
+++ src/app/subject/page.tsx
@@ -9,7 +9,6 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Button } from "@mui/material";
-import { MajorForm } from "../_components/majorForm";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { MajorSelectors } from "@/redux/features/major/majorSelectors";
 import { MajorThunks } from "@/redux/features/major/majorThunk";
