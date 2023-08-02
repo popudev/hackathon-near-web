e7b4f6f5f57267b934e7669361c91937d81b9221
popudev <phuthinh53.it@gmail.com>
2023-08-02 16:46:50 +0700

    feat: add dialog when register success

--- src/app/register/instructor/page.tsx
+++ src/app/register/instructor/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { AlertDialogSlide } from "@/app/_components/AlertDialogSlide";
 import { useAppSelector } from "@/redux/hooks";
 import { userService } from "@/services/user";
 import {
@@ -17,6 +18,7 @@ import { useState } from "react";
 export default function Register() {
   const { contract, isSignedIn, wallet } = useAppSelector((state) => state.web3);
   const [open, setOpen] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -38,6 +40,7 @@ export default function Register() {
           ...values,
         });
         await contract.registerInstructor();
+        setOpenDialog(true);
       }
 
       setOpen(false);
@@ -45,140 +48,148 @@ export default function Register() {
   });
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flex: 1,
-      }}
-    >
-      <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
+    <>
       <Box
         sx={{
           display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flex: 1,
         }}
       >
-        <Paper
-          variant="outlined"
+        <Box
           sx={{
-            padding: 4,
-            width: 550,
             display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
           }}
         >
-          <Box>
-            <Typography
-              variant="h1"
-              sx={{
-                fontSize: 40,
-                fontWeight: "500",
-                textAlign: "center",
-                mb: 4,
-              }}
-            >
-              Đăng ký giảng viên
-            </Typography>
-          </Box>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Họ và tên"
-                name="full_name"
-                onChange={(e) => (formik.initialValues.full_name = e.target.value)}
-                required
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                placeholder=""
-                label="Ngày sinh"
-                name="date_of_birth"
-                onChange={(e) => (formik.initialValues.date_of_birth = e.target.value)}
-                required
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                type="email"
-                fullWidth
-                label="Email"
-                name="email"
-                onChange={(e) => (formik.initialValues.email = e.target.value)}
-                required
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Số điện thoại"
-                name="phone"
-                onChange={(e) => (formik.initialValues.phone = e.target.value)}
-                required
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                fullWidth
-                label="Số CMND/CCCD"
-                name="national_identity_card"
-                onChange={(e) =>
-                  (formik.initialValues.national_identity_card = e.target.value)
-                }
-                required
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <TextField
-                fullWidth
-                label="Ngày cấp"
-                name="national_identity_card_date"
-                onChange={(e) =>
-                  (formik.initialValues.national_identity_card_date = e.target.value)
-                }
-                required
-                variant="outlined"
-              />
-            </Grid>
-          </Grid>
-          <Box
+          <Paper
+            variant="outlined"
             sx={{
-              mt: 4,
+              padding: 4,
+              width: 550,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
             }}
           >
-            <Button
-              onClick={() => formik.submitForm()}
-              variant="contained"
+            <Box>
+              <Typography
+                variant="h1"
+                sx={{
+                  fontSize: 40,
+                  fontWeight: "500",
+                  textAlign: "center",
+                  mb: 4,
+                }}
+              >
+                Đăng ký giảng viên
+              </Typography>
+            </Box>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Họ và tên"
+                  name="full_name"
+                  onChange={(e) => (formik.initialValues.full_name = e.target.value)}
+                  required
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  placeholder=""
+                  label="Ngày sinh"
+                  name="date_of_birth"
+                  onChange={(e) => (formik.initialValues.date_of_birth = e.target.value)}
+                  required
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  type="email"
+                  fullWidth
+                  label="Email"
+                  name="email"
+                  onChange={(e) => (formik.initialValues.email = e.target.value)}
+                  required
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Số điện thoại"
+                  name="phone"
+                  onChange={(e) => (formik.initialValues.phone = e.target.value)}
+                  required
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  fullWidth
+                  label="Số CMND/CCCD"
+                  name="national_identity_card"
+                  onChange={(e) =>
+                    (formik.initialValues.national_identity_card = e.target.value)
+                  }
+                  required
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <TextField
+                  fullWidth
+                  label="Ngày cấp"
+                  name="national_identity_card_date"
+                  onChange={(e) =>
+                    (formik.initialValues.national_identity_card_date = e.target.value)
+                  }
+                  required
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+            <Box
               sx={{
-                marginRight: 4,
-                fontSize: 18,
+                mt: 4,
               }}
             >
-              Đăng ký
-            </Button>
-            <Button
-              href="/"
-              sx={{
-                fontSize: 18,
-              }}
-            >
-              Hủy
-            </Button>
-          </Box>
-        </Paper>
+              <Button
+                onClick={() => formik.submitForm()}
+                variant="contained"
+                sx={{
+                  marginRight: 4,
+                  fontSize: 18,
+                }}
+              >
+                Đăng ký
+              </Button>
+              <Button
+                href="/"
+                sx={{
+                  fontSize: 18,
+                }}
+              >
+                Hủy
+              </Button>
+            </Box>
+          </Paper>
+        </Box>
       </Box>
-    </Box>
+      <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <AlertDialogSlide
+        open={openDialog}
+        title="Bạn đã đăng ký vào vị trí giảng viên thành công"
+        desrciption="Đơn đăng ký của bạn đã được gửi đến quản trị viên. Vui lòng kiểm tra email để nhận được thông báo"
+        onClose={() => {}}
+      />
+    </>
   );
 }
