9d3dc55657e54e7cb46ecc10364b31501fbaebe6
LuongThinh123 <luongchithinh123@gmail.com>
2023-08-02 21:02:57 +0700

    fix: confilc

--- src/app/_components/student/StudentSubjectItem.tsx
+++ src/app/_components/student/StudentSubjectItem.tsx
@@ -1,33 +1,5 @@
 "use client";
-
-import {
-  Box,
-  Button,
-  Card,
-  Grid,
-  Menu,
-  MenuItem,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { useFormik } from "formik";
-import Tab from "@mui/material/Tab";
-import TabContext from "@mui/lab/TabContext";
-import TabList from "@mui/lab/TabList";
-import TabPanel from "@mui/lab/TabPanel";
-import PopupState, { bindMenu, bindTrigger } from "material-ui-popup-state";
-import React, { useEffect, useState } from "react";
-// import { IconSettings } from "@tabler/icons";
-import SettingsIcon from "@mui/icons-material/Settings";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { Web3Thunks } from "@/redux/features/web3/web3Thunk";
-import { useRouter } from "next/navigation";
+import { TableCell, TableRow } from "@mui/material";
 
 // interface UserItemProps {
 //     student: User;
@@ -41,9 +13,6 @@ import { useRouter } from "next/navigation";
 export function StudentSubjectItem() {
   return (
     <TableRow>
-      {/* <TableCell align="left" sx={{ display: "none" }}>
-          {user?.code}
-        </TableCell> */}
       <TableCell align="center">Cấu trúc dữ liệu và giải thuật</TableCell>
       <TableCell align="center" sx={{}}>
         4
